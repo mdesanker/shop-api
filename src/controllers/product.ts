afba9554d4c8594e9bfae7390b5da381335913c6
Michael Desanker <mdesanker@gmail.com>
2022-02-07 14:36:31 -0500

    Add controller for get product by id route

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -9,7 +9,7 @@ const getAllProducts = async (
   try {
     const products = await Product.find({}).populate("category");
 
-    res.json(products);
+    return res.json(products);
   } catch (err: unknown) {
     if (err instanceof Error) {
       console.error(err.message);
@@ -18,4 +18,24 @@ const getAllProducts = async (
   }
 };
 
-export default { getAllProducts };
+const getProduct = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findById(id);
+
+    // Invalid product id
+    if (!product) {
+      return res.status(400).json({ errors: [{ msg: "Invalid product id" }] });
+    }
+
+    return res.json(product);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error(err.message);
+      return res.status(500).send("Server error");
+    }
+  }
+};
+
+export default { getAllProducts, getProduct };
--- src/routes/api/product.ts
+++ src/routes/api/product.ts
@@ -3,5 +3,6 @@ const product = express.Router();
 import productController from "../../controllers/product";
 
 product.get("/all", productController.getAllProducts);
+product.get("/:id", productController.getProduct);
 
 export = product;
