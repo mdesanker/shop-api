fc225be58cdadef74887f3374d1afca20f544ec1
Michael Desanker <mdesanker@gmail.com>
2022-02-09 05:50:41 -0500

    Refactor auth controllers into separate file

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,8 +1,59 @@
 import { Request, Response, NextFunction } from "express";
-import User from "../models/User";
+import passport from "passport";
 
 const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
   res.send("Auth GET controller test");
 };
 
-export default { getAllUsers };
+// Sign in fails
+const signinFailed = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.status(400).json({ msg: "Authentication failed" });
+};
+
+// Sign in successful
+const signinSuccessful = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log(req.user);
+  res.json({ msg: "Authentication successful" });
+};
+
+// Google authenticate
+const googleAuthenticate = passport.authenticate("google", {
+  scope: ["profile", "email"],
+});
+
+// Google redirect
+const googleRedirect = [
+  passport.authenticate("google", {
+    failureRedirect: "/auth/failed",
+    // session: false,
+    // successRedirect: "/auth/success",
+  }),
+  async (req: Request, res: Response) => {
+    // console.log(req.user);
+    // const token = req.user.generateJWT();
+    res.redirect("/auth/success");
+  },
+];
+
+// Logout
+const logout = async (req: Request, res: Response, next: NextFunction) => {
+  req.logout();
+  res.redirect("/");
+};
+
+export default {
+  getAllUsers,
+  signinFailed,
+  signinSuccessful,
+  googleAuthenticate,
+  googleRedirect,
+  logout,
+};
--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -1,52 +1,11 @@
-import express, { Request, Response, NextFunction } from "express";
-import passport from "passport";
+import express from "express";
 const auth = express.Router();
 import authController from "../../controllers/auth";
-import requireGoogleAuth from "../../middleware/requireGoogleAuth";
 
-interface AuthRequest extends Request {
-  user?: any;
-}
-
-auth.get("/", (req: Request, res: Response) => {
-  res.json({ msg: "You are not logged in" });
-});
-
-auth.get("/failed", (req: Request, res: Response) => {
-  res.json({ msg: "Failed" });
-});
-
-auth.get("/success", (req: AuthRequest, res: Response) => {
-  res.send(`Welcome ${req.user && req.user.email}`);
-  // res.send("Welcome");
-});
-
-auth.get(
-  "/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
-
-auth.get(
-  "/google/redirect",
-  passport.authenticate("google", {
-    failureRedirect: "/auth/failed",
-    // successRedirect: "/auth/success",
-  }),
-  (req: Request, res: Response) => {
-    res.redirect("/auth/success");
-    // res.send("You are logged in")
-  }
-);
-
-auth.get("/logout", async (req: Request, res: Response) => {
-  req.logout();
-  res.redirect("/");
-});
-
-auth.get("/test", requireGoogleAuth, async (req: Request, res: Response) =>
-  res.send("This is a protected route")
-);
+auth.get("/failed", authController.signinFailed);
+auth.get("/success", authController.signinSuccessful);
+auth.get("/google", authController.googleAuthenticate);
+auth.get("/google/redirect", authController.googleRedirect);
+auth.get("/logout", authController.logout);
 
 export = auth;
