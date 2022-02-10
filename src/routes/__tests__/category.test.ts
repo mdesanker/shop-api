7768de7d5b44bb8b5167e081ffc1279527c2aeed
Michael Desanker <mdesanker@gmail.com>
2022-02-10 13:25:51 -0500

    Add tests for get category by id route

--- src/routes/__tests__/category.test.ts
+++ src/routes/__tests__/category.test.ts
@@ -28,3 +28,20 @@ describe("GET /category/all", () => {
     expect(res.body.length).toBeGreaterThan(0);
   });
 });
+
+describe("GET /category/:id", () => {
+  it("return specific category", async () => {
+    const res = await request(app).get(`/category/${categoryId}`);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("name");
+  });
+
+  it("return error for invalid category id", async () => {
+    const res = await request(app).get(`/category/${invalidCategoryId}`);
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("errors");
+    expect(res.body.errors[0].msg).toEqual("Invalid category id");
+  });
+});
